c34f955519f52935549ed0cee76ae9f965d56faa
Lucas Mendonca <lucas.mendonca16@gmail.com>
2018-03-28 10:19:13 -0300

    Create MeuCEPHelper.js

---
+++ MeuCEPHelper.js
@@ -0,0 +1,10 @@
+({
+     limpaForms: function(component) {
+            // Limpa valores do formulário de cep.
+            component.set("v.newAddress.Logradouro", "");
+            component.set("v.newAddress.Numero", "");
+           	component.set("v.newAddress.Bairro", "");
+            component.set("v.newAddress.Cidade", "");
+            component.set("v.newAddress.UF", "");
+    }
+})
